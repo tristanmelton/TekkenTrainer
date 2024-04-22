df45241677e3a047b511769fcc573e1f5cc356f7
Tristan Melton <tristanmelton@engineering.ucla.edu>
2024-04-21 21:33:38 -0700

    First working version. Only includes mid blocks and lows. Calculates a rate of success at the end for each move.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 
 import threading
 import os
+import random 
+
 import cv2
 from inputs import get_gamepad
 
@@ -54,6 +56,9 @@ def handle_input():
         print(blocking_low, blocking_mid)
 
 def handle_video(move_set, move_id_list):
+
+    success_rates = {}
+    success_totals = {}
     for id in move_id_list:
         current_move = move_set[id]
         move_hit_info = current_move[1]
@@ -64,16 +69,15 @@ def handle_video(move_set, move_id_list):
         success_tot = len(hit_frame)
         successes = 0
 
-        cap = current_move[0]
+        cap = current_move[0][0]
+        framerate = current_move[0][1]
         # Read until video is completed
         frame_number = 0
         move_stage = 0
-        frame_b = []
         while(cap.isOpened()):
             # Capture frame-by-frame
             ret, frame = cap.read()
             if ret == True:
-                frame_b = frame
                 print(frame_number)
                 if frame_number in hit_frame:
                     match hit_loc[move_stage]:
@@ -86,7 +90,7 @@ def handle_video(move_set, move_id_list):
                     move_stage += 1
                 # Display the resulting frame
                 cv2.imshow('Frame',frame)
-                cv2.waitKey(int(1/30*1000))
+                cv2.waitKey(int(1/framerate*1000))
                 # Press Q on keyboard to  exit
                 #while not cv2.waitKey(25) & 0xFF == ord('q'):
                 #    pass
@@ -94,21 +98,35 @@ def handle_video(move_set, move_id_list):
             # Break the loop
             else: 
                 break
-        image = []
+        frame = np.zeros((int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),3),np.uint8)
         if successes == success_tot:
-            image = cv2.rectangle(frame_b, (0,0), (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))), thickness=-1, color=(0,255,0))
+            cv2.rectangle(frame, (0,0), (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))), thickness=-1, color=(0,255,0))
+            if id not in success_rates:
+                success_rates[id] = 0
+            success_rates[id] += 1
         else:
-            image = cv2.rectangle(frame_b, (0,0), (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))), thickness=-1, color=(0,0,255))
-        frame_max = 30
+            cv2.rectangle(frame, (0,0), (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))), thickness=-1, color=(0,0,255))
+            if id not in success_rates:
+                success_rates[id] = 0
+
+        frame_max = framerate
         frame_number = 0
         while frame_number < frame_max:
-                cv2.imshow('Frame', image)
-                cv2.waitKey(int(1/30*1000))
+                cv2.imshow('Frame', frame)
+                cv2.waitKey(int(1/framerate*1000))
                 frame_number += 1
-
+        if id not in success_totals:
+            success_totals[id] = 0
+        success_totals[id] += 1
 
         # When everything done, release the video capture object
-        cap.release()
+        #cap.release()
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        continue
+
+    rates = {k: float(success_rates[k])/success_totals[k] for k in success_totals}
+    #rates=dict(zip(move_set,list(rates.values()))) 
+    print(rates)
 
 def process_set(org_folder, video_folder):
     moves_processed = []
@@ -137,16 +155,22 @@ def process_set(org_folder, video_folder):
             print("Error opening video stream or file")
             exit(3)
         move_frames = []
-        for state in move_data[1::]:
+        for state in move_data[2::]:
             state_info = state.split('-')
             move_frames.append((state_info[0], state_info[1].replace('\n','')))
-        moves_processed.append((cap, move_frames))
+        moves_processed.append(((cap, int(move_data[1])), move_frames))
     return moves_processed
+
+
+
 
 moves_proc = process_set(folder, training_set)
 
+movelist_test = list(np.random.randint(low = 0,high=2,size=5))
+
+
 input_thread = threading.Thread(target=handle_input)
-video_thread = threading.Thread(target=handle_video, args=(moves_proc, [0,1]))
+video_thread = threading.Thread(target=handle_video, args=(moves_proc, movelist_test))
 input_thread.start()
 video_thread.start()
 
